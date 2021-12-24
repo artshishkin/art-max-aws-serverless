3a7dfa086a0bb0c04ebdf9075517a0552ad33ebb
Art <d.art.shishkin@gmail.com>
2021-12-24 18:30:19 +0200

    Store Data SAM modifications (#3)

--- compare-yourself/app-code/store-data.js
+++ compare-yourself/app-code/store-data.js
@@ -1,6 +1,27 @@
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB({region: 'eu-north-1', apiVersion: '2012-08-10'});
+
+const tableName = process.env.COMPARE_YOURSELF_TABLE;
+
 exports.handler = (event, context, callback) => {
-    let {name, age} = event;
-    let message = name + ' ' + age;
-    let result = {'message': message};
-    callback(null, result);
+
+    const params = {
+        TableName: tableName,
+        Item: {
+            'UserId': {S: "user_" + Math.random()},
+            'Age': {N: event.age},
+            'Height': {N: event.height},
+            'Income': {N: event.income}
+        }
+    };
+
+    dynamodb.putItem(params, function (err, data) {
+        if (err) {
+            console.log("Error", err);
+            callback(err);
+        } else {
+            console.log("Success", data);
+            callback(null, data);
+        }
+    });
 };
