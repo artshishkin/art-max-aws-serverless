cb49077c974c9fe6be37d302b3e71e3ca3d62ea7
Art <d.art.shishkin@gmail.com>
2021-12-24 14:34:09 +0200

    64. Accessing DynamoDB from Lambda (#3)

--- Section3ApiLambda/index.js
+++ Section3ApiLambda/index.js
@@ -1,3 +1,6 @@
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB({region: 'eu-north-1', apiVersion: '2012-08-10'});
+
 exports.handler = (event, context, callback) => {
     let {name, age} = event;
     let message = name + ' ' + age;
