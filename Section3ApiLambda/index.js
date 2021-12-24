8f07a950881769bd697020d9066d89e0ff4a3620
Art <d.art.shishkin@gmail.com>
2021-12-24 15:17:54 +0200

    66 Putting Items into a DynamoDB Table from Lambda (#3)

--- Section3ApiLambda/index.js
+++ Section3ApiLambda/index.js
@@ -2,8 +2,24 @@ const AWS = require('aws-sdk');
 const dynamodb = new AWS.DynamoDB({region: 'eu-north-1', apiVersion: '2012-08-10'});
 
 exports.handler = (event, context, callback) => {
-    let {name, age} = event;
-    let message = name + ' ' + age;
-    let result = {'message': message};
-    callback(null, result);
+
+    const params = {
+        TableName: 'compare-yourself',
+        Item: {
+            'UserId': {S: "blabla7387483"},
+            'Age': {N: "38"},
+            'Height': {N: "180"},
+            'Income': {N: "2500"}
+        }
+    };
+
+    dynamodb.putItem(params, function (err, data) {
+        if (err) {
+            console.log("Error", err);
+            callback(err);
+        } else {
+            console.log("Success", data);
+            callback(null, data);
+        }
+    });
 };
