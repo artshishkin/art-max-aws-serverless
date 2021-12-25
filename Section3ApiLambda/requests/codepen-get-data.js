3bf13987e0e1b8c8632519408079fd53629d6d94
Art <d.art.shishkin@gmail.com>
2021-12-25 16:05:48 +0200

    74. Testing it from the Web & Passing Correct Data (#3)

--- Section3ApiLambda/requests/codepen-get-data.js
+++ Section3ApiLambda/requests/codepen-get-data.js
@@ -1,6 +1,6 @@
 var xhr = new XMLHttpRequest();
 xhr.open('GET','https://9cxvvjl4d0.execute-api.eu-north-1.amazonaws.com/dev/compare-yourself/all');
 xhr.onreadystatechange = function(event) {
-    console.log(event.target.response);
+    console.log(JSON.parse(event.target.response));
 }
 xhr.send();
--- compare-yourself/events/codepen-get-data.js
+++ compare-yourself/events/codepen-get-data.js
@@ -1,6 +1,6 @@
 var xhr = new XMLHttpRequest();
-xhr.open('GET','https://6zlxd52ys6.execute-api.eu-north-1.amazonaws.com/Prod/compare-yourself/all');
+xhr.open('GET','https://6zlxd52ys6.execute-api.eu-north-1.amazonaws.com/Prod/compare-yourself/single');
 xhr.onreadystatechange = function(event) {
-    console.log(event.target.response);
+    console.log(JSON.parse(event.target.response));
 }
 xhr.send();
