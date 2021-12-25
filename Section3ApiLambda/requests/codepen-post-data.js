230232631eb15e67031d9a41d6e053f32a7d1591
Art <d.art.shishkin@gmail.com>
2021-12-25 23:53:05 +0200

    86. Using Custom Authorizers (#6)

--- Section3ApiLambda/requests/codepen-post-data.js
+++ Section3ApiLambda/requests/codepen-post-data.js
@@ -4,4 +4,5 @@ xhr.onreadystatechange = function (event) {
     console.log(event.target.response);
 }
 xhr.setRequestHeader('Content-Type', 'application/json');
+xhr.setRequestHeader('Authorization', 'allow');
 xhr.send(JSON.stringify({age: 32, height: 175, income: 1500}));
