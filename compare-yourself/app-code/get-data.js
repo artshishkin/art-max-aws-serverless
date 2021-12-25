8689f77cda0ca2f87da501491069768725a0c424
Art <d.art.shishkin@gmail.com>
2021-12-25 11:36:26 +0200

    70.2 Scanning Data in DynamoDB from Lambda - SAM (#3)

--- compare-yourself/app-code/get-data.js
+++ compare-yourself/app-code/get-data.js
@@ -1,25 +1,41 @@
-exports.handler = async (event) => {
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB({region: 'eu-north-1', apiVersion: '2012-08-10'});
+
+const tableName = process.env.COMPARE_YOURSELF_TABLE;
+
+exports.handler = (event, context, callback) => {
 
     const type = event.type;
 
     let response;
 
-    if (type ==='all') {
-        response = {
-            statusCode: 200,
-            body: 'Return all results'
-        };
-    } else if (type === 'single'){
+    const params = {
+        TableName: tableName
+    };
+
+    if (type === 'all') {
+
+        dynamodb.scan(params, function (err, data) {
+            if (err) {
+                console.log("Error", err);
+                callback(err);
+            } else {
+                console.log("Success", data);
+                callback(null, data);
+            }
+        });
+
+    } else if (type === 'single') {
         response = {
             statusCode: 200,
             body: 'Return single result'
         };
+        callback(null, response);
     } else {
         response = {
             statusCode: 400,
             body: 'Wrong type parameter: ' + type + '. Must be `all` or `single`.'
         };
+        callback(null, response);
     }
-
-    return response;
 };
