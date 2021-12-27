b417c0d89e98ed450e60714ea142601300c10a39
Art <d.art.shishkin@gmail.com>
2021-12-27 18:29:22 +0200

    101.2 Passing the right User ID to Lambda - Frontend Source Code (#6)

--- frontend-app/src/app/compare/compare.service.ts
+++ frontend-app/src/app/compare/compare.service.ts
@@ -14,6 +14,7 @@ export class CompareService {
   dataLoaded = new Subject<CompareData[]>();
   dataLoadFailed = new Subject<boolean>();
   userData: CompareData;
+
   constructor(private http: Http,
               private authService: AuthService) {
   }
@@ -23,8 +24,14 @@ export class CompareService {
     this.dataIsLoading.next(true);
     this.dataEdited.next(false);
     this.userData = data;
-      this.http.post('https://API_ID.execute-api.REGION.amazonaws.com/dev/', data, {
-        headers: new Headers({'Authorization': 'XX'})
+    this.authService.getAuthenticatedUser().getSession((err, session) => {
+      if (err) {
+        return;
+      }
+      let idToken = session.getIdToken().getJwtToken();
+
+      this.http.post('https://9cxvvjl4d0.execute-api.eu-north-1.amazonaws.com/dev/compare-yourself', data, {
+        headers: new Headers({'Authorization': idToken})
       })
         .subscribe(
           (result) => {
@@ -38,51 +45,55 @@ export class CompareService {
             this.dataEdited.next(false);
           }
         );
+    });
+
   }
+
   onRetrieveData(all = true) {
     this.dataLoaded.next(null);
     this.dataLoadFailed.next(false);
-      let queryParam = '';
-      let urlParam = 'all';
-      if (!all) {
-        urlParam = 'single';
-      }
-      this.http.get('https://API_ID.execute-api.REGION.amazonaws.com/dev/' + urlParam + queryParam, {
-        headers: new Headers({'Authorization': 'XXX'})
-      })
-        .map(
-          (response: Response) => response.json()
-        )
-        .subscribe(
-          (data) => {
-            if (all) {
-              this.dataLoaded.next(data);
-            } else {
-              console.log(data);
-              if (!data) {
-                this.dataLoadFailed.next(true);
-                return;
-              }
-              this.userData = data[0];
-              this.dataEdited.next(true);
+    let queryParam = '';
+    let urlParam = 'all';
+    if (!all) {
+      urlParam = 'single';
+    }
+    this.http.get('https://API_ID.execute-api.REGION.amazonaws.com/dev/' + urlParam + queryParam, {
+      headers: new Headers({'Authorization': 'XXX'})
+    })
+      .map(
+        (response: Response) => response.json()
+      )
+      .subscribe(
+        (data) => {
+          if (all) {
+            this.dataLoaded.next(data);
+          } else {
+            console.log(data);
+            if (!data) {
+              this.dataLoadFailed.next(true);
+              return;
             }
-          },
-          (error) => {
-            this.dataLoadFailed.next(true);
-            this.dataLoaded.next(null);
+            this.userData = data[0];
+            this.dataEdited.next(true);
           }
-        );
+        },
+        (error) => {
+          this.dataLoadFailed.next(true);
+          this.dataLoaded.next(null);
+        }
+      );
   }
+
   onDeleteData() {
     this.dataLoadFailed.next(false);
-      this.http.delete('https://API_ID.execute-api.REGION.amazonaws.com/dev/', {
-        headers: new Headers({'Authorization': 'XXX'})
-      })
-        .subscribe(
-          (data) => {
-            console.log(data);
-          },
-          (error) => this.dataLoadFailed.next(true)
-        );
+    this.http.delete('https://API_ID.execute-api.REGION.amazonaws.com/dev/', {
+      headers: new Headers({'Authorization': 'XXX'})
+    })
+      .subscribe(
+        (data) => {
+          console.log(data);
+        },
+        (error) => this.dataLoadFailed.next(true)
+      );
   }
 }
